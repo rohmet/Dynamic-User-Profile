8ff7d4769f38d27339c737b6a087a3e87e5a06d4
rohmet <harahmanabdarib@gmail.com>
2025-07-13 09:22:57 +0800

    Feat: Added a form to dynamically add new hobbies

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
@@ -8,15 +9,24 @@ app.get('/', (req, res) => {
   res.render('index', { title: 'Welcome', message: 'Hello, EJS!' });
 });
 
+const profil = {
+  nama: 'Lee jeno',
+  email: 'jeno@gmail.com',
+  // hobi: ['Membaca', 'Koding', 'Musik']
+  hobi: []
+};
+
 app.get('/profil', (req, res) => {
-  const profil = {
-    nama: 'Lee jeno',
-    email: 'jeno@gmail.com',
-    // hobi: ['Membaca', 'Koding', 'Musik']
-    hobi: []
-  };
   res.render('profil', { title:'Profil', profil });
 });
+
+app.post('/tambah-hobi', (req, res) => {
+  const hobiBaru = req.body.hobiBaru;
+  if (hobiBaru){
+    profil.hobi.push(hobiBaru);
+  }
+  res.redirect('/profil');
+})
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
