ca48c028f524f7ad57f5155c42bd6975a54ec75d
rohmet <harahmanabdarib@gmail.com>
2025-07-13 10:01:25 +0800

    "Refactor: Change profile data fetching logic to async/await"

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.use(express.urlencoded({ extended: true }));
+const profil = require('./data');
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
@@ -9,15 +11,14 @@ app.get('/', (req, res) => {
   res.render('index', { title: 'Welcome', message: 'Hello, EJS!' });
 });
 
-const profil = {
-  nama: 'Lee jeno',
-  email: 'jeno@gmail.com',
-  // hobi: ['Membaca', 'Koding', 'Musik']
-  hobi: []
-};
-
-app.get('/profil', (req, res) => {
-  res.render('profil', { title:'Profil', profil });
+app.get('/profil', async (req, res) => {
+  try {
+    const dataProfil = await ambilDataProfil();
+    res.render('profil', { title: 'Profil', profil: dataProfil });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Terjadi kesalahan pada server");
+  }
 });
 
 app.post('/tambah-hobi', (req, res) => {
@@ -27,6 +28,14 @@ app.post('/tambah-hobi', (req, res) => {
   }
   res.redirect('/profil');
 })
+
+function ambilDataProfil() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(profil);
+    }, 1500);
+  });
+}
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
